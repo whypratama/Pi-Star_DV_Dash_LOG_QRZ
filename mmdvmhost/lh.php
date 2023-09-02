e0e36f96f9bc3d87bead4c64bee510b789232229
whypratama <42627915+whypratama@users.noreply.github.com>
2023-09-02 10:54:53 +0700

    Update lh.php
    Menambahkan Tombol Log, Kolom Nama dan Negara

--- mmdvmhost/lh.php
+++ mmdvmhost/lh.php
@@ -1,3 +1,4 @@
+
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'].'/config/config.php';          // MMDVMDash Config
 include_once $_SERVER['DOCUMENT_ROOT'].'/mmdvmhost/tools.php';        // MMDVMDash Tools
@@ -26,19 +27,42 @@ $idLookupUrl = "https://database.radioid.net/database/view?id=";
 if ($callsignLookupSvc == "RadioID") { $callsignLookupUrl = "https://database.radioid.net/database/view?callsign="; }
 if ($callsignLookupSvc == "QRZ") { $callsignLookupUrl = "https://www.qrz.com/db/"; }
 
+
+
 ?>
+
+<?php
+// Baca data dari file CSV
+$csvFile = '/var/www/dashboard/user.csv';
+$csvData = [];
+
+if (($handle = fopen($csvFile, 'r')) !== false) {
+    while (($row = fgetcsv($handle)) !== false) {
+        $csvData[$row[1]] = [
+            'first_name' => $row[2],
+            'last_name' => $row[3],
+            'country' => $row[6]
+        ];
+    }
+    fclose($handle);
+}
+?>
+
 <b><?php echo $lang['last_heard_list'];?></b>
   <table>
     <tr>
       <th><a class="tooltip" href="#"><?php echo $lang['time'];?> (<?php echo date('T')?>)<span><b>Time in <?php echo date('T')?> time zone</b></span></a></th>
       <th><a class="tooltip" href="#"><?php echo $lang['mode'];?><span><b>Transmitted Mode</b></span></a></th>
       <th style="min-width:14ch"><a class="tooltip" href="#"><?php echo $lang['callsign'];?><span><b>Callsign</b></span></a></th>
+      <th><a class="tooltip" href="#">Name<span><b>Name</b></span></a></th>  
+      <th><a class="tooltip" href="#">Country<span><b>Country</b></span></a></th>
       <th><a class="tooltip" href="#"><?php echo $lang['target'];?><span><b>Target, D-Star Reflector, DMR Talk Group etc</b></span></a></th>
       <th><a class="tooltip" href="#"><?php echo $lang['src'];?><span><b>Received from source</b></span></a></th>
       <th><a class="tooltip" href="#"><?php echo $lang['dur'];?>(s)<span><b>Duration in Seconds</b></span></a></th>
       <th><a class="tooltip" href="#"><?php echo $lang['loss'];?><span><b>Packet Loss</b></span></a></th>
       <th><a class="tooltip" href="#"><?php echo $lang['ber'];?><span><b>Bit Error Rate</b></span></a></th>
-    </tr>
+      <th><a class="tooltip" href="#">QRZ<span><b>Log to QRZ</b></span></a></th>
+</tr>
 <?php
 $i = 0;
 for ($i = 0;  ($i <= 19); $i++) { //Last 20 calls
@@ -51,10 +75,21 @@ for ($i = 0;  ($i <= 19); $i++) { //Last 20 calls
                         $dt = new DateTime($utc_time, $utc_tz);
                         $dt->setTimeZone($local_tz);
                         $local_time = $dt->format('H:i:s M jS');
+    
+            // Periksa apakah data sudah ada di dalam $csvData
+            $callsign = $listElem[2];
+            $name = '';
+            $country = '';
+        
+            if (isset($csvData[$callsign])) {
+                $name = $csvData[$callsign]['first_name'] . ' ' . $csvData[$callsign]['last_name'];
+                $country = $csvData[$callsign]['country'];
+                }
+        
 		echo "<tr>";
 		echo "<td align=\"left\">$local_time</td>";
 		echo "<td align=\"left\">".str_replace('Slot ', 'TS', $listElem[1])."</td>";
-		if (is_numeric($listElem[2])) {
+               if (is_numeric($listElem[2])) {
 			if ($listElem[2] > 9999) { echo "<td align=\"left\"><a href=\"".$idLookupUrl.$listElem[2]."\" target=\"_blank\">$listElem[2]</a></td>"; }
 			else { echo "<td align=\"left\">".$listElem[2]."</td>"; }
 		} elseif (!preg_match('/[A-Za-z].*[0-9]|[0-9].*[A-Za-z]/', $listElem[2])) {
@@ -67,6 +102,13 @@ for ($i = 0;  ($i <= 19); $i++) { //Last 20 calls
 				echo "<td align=\"left\"><div style=\"float:left;\"><a href=\"".$callsignLookupUrl.$listElem[2]."\" target=\"_blank\">$listElem[2]</a></div> <div style=\"text-align:right;\">&#40;<a href=\"https://aprs.fi/#!call=".$listElem[2]."*\" target=\"_blank\">GPS</a>&#41;</div></td>";
 			}
 		}
+
+               // Kolom Nama
+               echo "<td align=\"left\">$name</td>";
+            
+               // Kolom Negara
+               echo "<td align=\"left\">$country</td>";
+
 
 		if (strlen($listElem[4]) == 1) { $listElem[4] = str_pad($listElem[4], 8, " ", STR_PAD_LEFT); }
 		if ( substr($listElem[4], 0, 6) === 'CQCQCQ' ) {
@@ -97,7 +139,7 @@ for ($i = 0;  ($i <= 19); $i++) { //Last 20 calls
 		} else {
 			echo "<td>$listElem[6]</td>";
 
-			// Colour the Loss Field
+  		// Colour the Loss Field
 			if (floatval($listElem[7]) < 1) { echo "<td>$listElem[7]</td>"; }
 			elseif (floatval($listElem[7]) == 1) { echo "<td style=\"background:#1d1;\">$listElem[7]</td>"; }
 			elseif (floatval($listElem[7]) > 1 && floatval($listElem[7]) <= 3) { echo "<td style=\"background:#fa0;\">$listElem[7]</td>"; }
@@ -109,10 +151,92 @@ for ($i = 0;  ($i <= 19); $i++) { //Last 20 calls
 			elseif (floatval($listElem[8]) >= 2.0 && floatval($listElem[8]) <= 4.9) { echo "<td style=\"background:#fa0;\">$listElem[8]</td>"; }
 			else { echo "<td style=\"background:#f33;\">$listElem[8]</td>"; }
 		}
-		echo "</tr>\n";
-		}
+
+               // Kolom tombol "LOG"
+               echo '<td><button onclick="logToQRZ(\'' . $listElem[2] . '\')">LOG</button></td>';
+           
+               echo "</tr>\n";
+
+
+
+}
 	}
 }
 
+
+
+
+
 ?>
   </table>
+
+<script>
+function logToQRZ(callsign){
+    // Ganti dengan URL API QRZ.com yang sesuai
+    var apiUrl = 'https://logbook.qrz.com/api';
+
+    // Mengambil waktu saat ini dalam format yang sesuai
+    var utcDate = new Date();
+    var year = utcDate.getUTCFullYear();
+    var month = (utcDate.getUTCMonth() + 1).toString().padStart(2, '0');
+    var day = utcDate.getUTCDate().toString().padStart(2, '0');
+    var hours = utcDate.getUTCHours().toString().padStart(2, '0');
+    var minutes = utcDate.getUTCMinutes().toString().padStart(2, '0');
+
+
+    // Format pesan sukses
+    var successMessage = `${callsign} Jam ${hours}:${minutes} Tanggal ${year}-${month}-${day} Logged !`;
+
+    // Membuat objek QSO dalam format ADIF
+    var qsoData = `
+        <band:4>70cm
+        <band_rx:4>70cm
+        <freq:5>434.1
+        <freq_rx:5>434.1
+        <rst_rcvd:2>59
+        <rst_sent:2>59
+        <station_callsign:6>YC7OIZ
+        <tx_pwr:3>0.1        
+        <mode:12>DIGITALVOICE
+        <call:${callsign.length}>${callsign}
+        <qso_date:8>${year}${month}${day}
+        <time_on:4>${hours}${minutes}
+        <eor>`;
+
+    // Mengganti dengan kunci API QRZ.com Anda
+    var apiKey = '****-****-****-****';
+
+    // Mengirim permintaan POST ke API QRZ.com
+    var data = {
+        KEY: apiKey,
+        ACTION: 'INSERT',
+        ADIF: qsoData
+    };
+
+    fetch(apiUrl, {
+        method: 'POST',
+        body: new URLSearchParams(data),
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    })
+    .then(response => response.text())
+    .then(result => {
+        // Di sini Anda dapat menangani respons dari API QRZ.com
+        console.log(result);
+        alert(successMessage);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('Gagal mengirim data ke QRZ.com');
+    });
+}
+</script>
+
+<script>
+function getUserDataFromRadioid($callsign2) {
+    $url = "https://radioid.net/api/dmr/user/?callsign=" . urlencode($callsign2);
+    $response = file_get_contents($url);
+    return json_decode($response, true)['results'][0];
+}
+</script>
